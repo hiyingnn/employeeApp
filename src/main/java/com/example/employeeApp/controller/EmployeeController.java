ce1db7083f6eaf323da515e38c88118652fffeb6
hiyingnn <r.ying_98@hotmail.com>
2021-06-14 20:05:33 +0800

    Initialised front-end react-app.

--- src/main/java/com/example/employeeApp/controller/EmployeeController.java
+++ src/main/java/com/example/employeeApp/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.example.employeeApp.model.Employee;
 import com.example.employeeApp.model.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
